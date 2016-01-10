43c8e5342e0fce0d3d38830ce67c272308472fc8
Chris Gray <chris.gray@kiffer.ltd.uk>
2016-01-10 09:52:29 +0000

    Add new model preferences window.

---
+++ Keratoconus-Application/src/be/uza/keratoconus/userprefs/impl/ModelPreferencesWindow.java
@@ -0,0 +1,142 @@
+package be.uza.keratoconus.userprefs.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.Toggle;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+
+import org.osgi.framework.Constants;
+import org.osgi.service.component.ComponentContext;
+import org.osgi.service.log.LogService;
+
+import aQute.bnd.annotation.component.Activate;
+import aQute.bnd.annotation.component.Component;
+import aQute.bnd.annotation.component.Reference;
+import be.uza.keratoconus.model.api.AvailableModelsService;
+import be.uza.keratoconus.userprefs.api.PreferencesWindow;
+
+@Component(properties = "rank=5")
+public class ModelPreferencesWindow extends AbstractPreferencesWindow implements
+		PreferencesWindow {
+
+	private LogService logService;
+
+	private AvailableModelsService availableModelsService;
+
+	private String selectedModelName;
+
+	public ModelPreferencesWindow() {
+		super("Classification model",
+				"Choose the classification model from among those available:",
+				"The application must be restarted in order for the new choice to take effect.");
+	}
+
+	@Reference
+	protected void setAvailableModelsService(AvailableModelsService ams) {
+		this.availableModelsService = ams;
+	}
+
+	@Reference
+	protected void setLogService(LogService ls) {
+		this.logService = ls;
+	}
+
+	@Activate
+	protected void activate(ComponentContext context,
+			Map<String, Object> properties) {
+		List<String> availableModelNames = availableModelsService
+				.getAvailableModelNames();
+		if (availableModelNames.size() < 2) {
+			logService.log(context.getServiceReference(), LogService.LOG_INFO,
+					"Found only one model so let's use it: "
+							+ availableModelNames.get(0));
+			context.disableComponent((String) properties
+					.get(Constants.SERVICE_PID));
+		} else {
+			logService.log(context.getServiceReference(), LogService.LOG_INFO,
+					"Available model names: " + availableModelNames);
+		}
+
+	}
+
+	@Override
+	public Pane createContent() {
+		List<String> availableModelNames = availableModelsService
+				.getAvailableModelNames();
+		String defaultModelName = availableModelNames.get(0);
+		selectedModelName = prefs.getSelectedModelName();
+		if (selectedModelName == null) {
+			selectedModelName = defaultModelName;
+			logService.log(LogService.LOG_INFO,
+					"No model selected in user preferences, defaulting to: "
+							+ selectedModelName);
+		} else {
+			logService.log(LogService.LOG_INFO,
+					"Model selected in user preferences: " + selectedModelName);
+			if (!availableModelNames.contains(selectedModelName)) {
+				logService.log(LogService.LOG_WARNING,
+						"Model selected in user preferences: "
+								+ selectedModelName
+								+ " is not available, defaulting to: "
+								+ defaultModelName);
+				selectedModelName = defaultModelName;
+			}
+		}
+
+		final List<RadioButton> buttons = new ArrayList<>();
+		try {
+			availableModelsService.selectModel(selectedModelName);
+
+			final ToggleGroup tg = new ToggleGroup();
+			for (String modelName : availableModelsService
+					.getAvailableModelNames()) {
+				String title = modelName.substring(0, 1)
+						+ modelName.substring(1).toLowerCase();
+				RadioButton rb = new RadioButton();
+				Text titleText = new Text(title);
+				titleText.getStyleClass().add(STYLE_CLASS_TITLE);
+				Label descriptionLabel = new Label(
+						availableModelsService.getModeDescription(modelName));
+				descriptionLabel.getStyleClass().add(STYLE_CLASS_DESCRIPTION);
+				rb.setGraphic(new VBox(titleText, descriptionLabel));
+				rb.getStyleClass().add(STYLE_CLASS_BUTTON);
+				rb.setToggleGroup(tg);
+				rb.setUserData(modelName);
+				if (modelName.equals(selectedModelName)) {
+					rb.setSelected(true);
+					rb.requestFocus();
+				}
+				buttons.add(rb);
+
+			}
+			tg.selectedToggleProperty().addListener(
+					new ChangeListener<Toggle>() {
+
+						@Override
+						public void changed(
+								ObservableValue<? extends Toggle> obs,
+								Toggle oldval, Toggle newval) {
+							if (newval != null) {
+								prefs.configureSelectedModelName((String) newval
+										.getUserData());
+							}
+
+						}
+					});
+		} catch (Exception e) {
+			logService.log(LogService.LOG_ERROR,
+					"Failed to delect classification model");
+		}
+
+		return new VBox(buttons.toArray(new RadioButton[buttons.size()]));
+	}
+}
