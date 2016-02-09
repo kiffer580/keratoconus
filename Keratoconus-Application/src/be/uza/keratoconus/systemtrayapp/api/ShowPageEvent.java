f7d18e4ec2aefbeb1c9ee66e061657593c9820b3
Chris Gray <chris.gray@kiffer.ltd.uk>
2016-02-09 12:41:35 +0000

    New event class.

---
+++ Keratoconus-Application/src/be/uza/keratoconus/systemtrayapp/api/ShowPageEvent.java
@@ -0,0 +1,16 @@
+package be.uza.keratoconus.systemtrayapp.api;
+
+import java.util.HashMap;
+
+import org.osgi.service.event.Event;
+
+public class ShowPageEvent extends Event {
+	public ShowPageEvent(String path, String title) {
+		super(HtmlViewerService.SHOWPAGE_TOPIC, new HashMap<String,Object>() {
+			{
+				put(HtmlViewerService.SHOWPAGE_PATH, path);
+				put(HtmlViewerService.SHOWPAGE_TITLE, title);
+			}
+		});
+	}
+}
