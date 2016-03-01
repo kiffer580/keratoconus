49d607e88b7b4576b6cb005e7d8e9c343b961ab4
Chris Gray <chris.gray@kiffer.ltd.uk>
2016-03-01 21:24:07 +0000

    Add GPL3 copyright notices to all fies which depende directly or indirectly on WEKA.

--- Keratoconus-Model-Impl/src/be/uza/keratoconus/model/impl/ModelConstants.java
+++ Keratoconus-Model-Impl/src/be/uza/keratoconus/model/impl/ModelConstants.java
@@ -1,3 +1,21 @@
+/*
+    This file is part of Keratoconus Assistant.
+
+    Keratoconus Assistant is free software: you can redistribute it 
+    and/or modify it under the terms of the GNU General Public License 
+    as published by the Free Software Foundation, either version 3 of 
+    the License, or (at your option) any later version.
+
+    Keratoconus Assistant is distributed in the hope that it will be 
+    useful, but WITHOUT ANY WARRANTY; without even the implied warranty 
+    of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Keratoconus Assistant.  If not, see 
+    <http://www.gnu.org/licenses/>.
+ */
+
 package be.uza.keratoconus.model.impl;
 
 public abstract class ModelConstants {
